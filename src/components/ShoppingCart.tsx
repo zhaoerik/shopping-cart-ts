92127aaee8dbd16c0720acc93077ddc5fc88aaae
zhaoerik <jediofthenyy@gmail.com>
2024-06-20 11:39:22 -0400

    initial commit

---
+++ src/components/ShoppingCart.tsx
@@ -0,0 +1,42 @@
+import {
+  Offcanvas,
+  OffcanvasBody,
+  OffcanvasHeader,
+  OffcanvasTitle,
+  Stack,
+} from "react-bootstrap";
+import { useShoppingCart } from "../context/ShoppingCartContext";
+import { CartItem } from "./CartItem";
+import { formatCurrency } from "../utils/formatCurrency";
+import storeItems from "../data/items.json";
+
+type ShoppingCartProps = {
+  isOpen: boolean;
+};
+
+export function ShoppingCart({ isOpen }: ShoppingCartProps) {
+  const { closeCart, cartItems } = useShoppingCart();
+  return (
+    <Offcanvas show={isOpen} onHide={closeCart} placement="end">
+      <OffcanvasHeader closeButton>
+        <OffcanvasTitle>Cart</OffcanvasTitle>
+      </OffcanvasHeader>
+      <OffcanvasBody>
+        <Stack gap={3}>
+          {cartItems.map((item) => (
+            <CartItem key={item.id} {...item} />
+          ))}
+          <div className="ms-auto fw-bold fs-5">
+            Total{" "}
+            {formatCurrency(
+              cartItems.reduce((total: number, cartItem) => {
+                const item = storeItems.find((item) => item.id === cartItem.id);
+                return total + (item?.price || 0) * cartItem.quantity;
+              }, 0)
+            )}
+          </div>
+        </Stack>
+      </OffcanvasBody>
+    </Offcanvas>
+  );
+}
